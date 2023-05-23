08098ee4e52d86eb09d4b73380bd4cf60fdc5221
Ozdemir-B <berkayozdemirv2@gmail.com>
2023-05-24 01:12:00 +0300

    PrototypeV1

--- SnakeUI/ContentView.swift
+++ SnakeUI/ContentView.swift
@@ -8,14 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    let timerAddSnake = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State var direction:String = "right"
+    
+    @State var dragPosition:CGSize = CGSize.zero
+    
+    @State var dragStartLocation:CGPoint = CGPoint.zero
+    @State var dragEndLocation:CGPoint = CGPoint.zero
+    
+    @State var snake = AllSnake()
+    
+    @State var applePosition = CGSize.zero
+    
+    func randomApplePosition() -> CGSize {
+        
+        let random_width:CGFloat = Array(stride(from: 100 as? CGFloat ?? CGFloat.zero, to: UIScreen.main.bounds.size.width/2 , by: 1)).shuffled().first!
+        
+        let random_height:CGFloat = Array(stride(from: 100 as? CGFloat ?? CGFloat.zero, to: UIScreen.main.bounds.size.height/2, by: 1)).shuffled().first!
+        let return_val = CGSize(width: random_width, height: random_height)
+        print("apple_position : \(return_val)")
+        print("device : \(UIScreen.main.bounds.size)")
+        return return_val
+        
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.edgesIgnoringSafeArea(.all)
+            ZStack{
+                ZStack{
+                    
+                    
+                    snake.renderSnake
+                    
+                    RoundedRectangle(cornerRadius: 0).frame(width:50,height:50).offset(self.applePosition).foregroundColor(.blue)
+                }
+                
+                
+            }
+            
         }
-        .padding()
+        .onAppear{
+            self.applePosition = self.randomApplePosition()
+        }
+        .onTapGesture(perform: {
+            //snake.addToTail()
+        })
+        .gesture(
+            DragGesture().onChanged{
+                value in
+                //dragPosition = value.translation
+                
+                dragStartLocation = value.startLocation
+                dragEndLocation = value.location
+                
+            }.onEnded{
+                value in
+                /*withAnimation(){
+                    dragPosition = CGSize.zero
+                    
+                }*/
+                let y_dif = dragEndLocation.x - dragStartLocation.x
+                let x_dif = dragEndLocation.y - dragStartLocation.y
+                if abs(x_dif) > abs(y_dif){
+                    if x_dif > 0{
+                        self.direction = "down"
+                    } else {
+                        self.direction = "up"
+                    }
+                } else {
+                    if y_dif > 0 {
+                        self.direction = "right"
+                    } else {
+                        self.direction = "left"
+                    }
+                }
+            })
+        .onReceive(timer, perform: {
+            time in
+            //print(self.direction)
+            
+            snake.move(newDirection: self.direction)
+            
+            if snake.didEatApple(applePosition: self.applePosition){
+                self.applePosition = self.randomApplePosition()
+                snake.addToTail()
+            }
+            
+            
+            
+        })
+        .onReceive(timerAddSnake, perform: {
+            time in
+            
+            //print("tail added")
+            //print(snake.parts.count)
+        })
+        
+        
     }
 }
 
---
+++ SnakeUI/PartSnake.swift
@@ -0,0 +1,141 @@
+//
+//  PartSnake.swift
+//  SnakeUI
+//
+//  Created by Berkay Ozdemir on 23.05.2023.
+//
+
+import Foundation
+
+import SwiftUI
+
+
+struct PartSnake{
+    var index:Int
+    var position:CGSize
+    var direction:String
+    
+    var renderPart:some View{
+        return Circle().frame(width:20,height:20)
+    }
+}
+
+struct AllSnake{
+    
+    var parts:[PartSnake] = [PartSnake(index: 0, position: CGSize.zero, direction: "right")]
+    
+    
+    var renderSnake:some View{
+        ForEach(parts, id:\.index){
+                part in
+                if part.index == 0{
+                    part.renderPart.offset(part.position).foregroundColor(.red)
+                } else {
+                    part.renderPart.offset(part.position).foregroundColor(.black)
+                }
+                
+                
+            }
+    }
+    
+    
+    mutating func addToTail() {
+        if let last = self.parts.last{
+                
+            var new = last
+            //print("--before.posititon : \(new.position)")
+            //completion()
+            new.index += 1
+            /*if new.direction == "right"{
+                new.position.width += 20
+            } else if new.direction == "left"{
+                new.position.width -= 20
+            } else if new.direction == "up"{
+                new.position.height += 20
+            } else {
+                new.position.height -= 20
+            }*/
+            self.parts.append(new)
+            
+        } else {
+            print("no last tail")
+            //completion()
+        }
+        
+    }
+    
+    mutating func move(newDirection:String){
+        self.swipePositionsAndDirections()
+        self.moveHead(newDirection: newDirection)//,newPosition: newPosition)
+    }
+    
+    mutating func moveHead(newDirection:String){
+        
+        if let p = parts.first{
+            var old = p
+            if p.direction == "up" && newDirection != "down"{
+                self.parts[0].direction = newDirection
+                self.parts[0].position.height -= 10
+            } else if p.direction == "down" && newDirection != "up"{
+                self.parts[0].direction = newDirection
+                self.parts[0].position.height += 10
+            } else if p.direction == "right" && newDirection != "left"{
+                self.parts[0].direction = newDirection
+                self.parts[0].position.width += 10
+            } else if p.direction == "left" && newDirection != "right"{
+                self.parts[0].direction = newDirection
+                self.parts[0].position.width -= 10
+            }
+            
+            
+            
+            //return old
+        } else {
+            //return nil
+        }
+        
+    }
+    
+    mutating func swipePositionsAndDirections() {
+        //rint("count : \(self.parts.count)")
+        let old_parts = self.parts
+        for i in 1..<parts.count{
+            //print("\(parts[i-1].position) -- \(parts[i].position)")
+            self.parts[i].position = old_parts[i-1].position
+            self.parts[i].direction = old_parts[i-1].direction
+            //self.parts[i].position = self.parts[i-1].position
+            //self.parts[i].direction = self.parts[i-1].direction
+        }
+        //print("---- count : \(self.parts.count)")
+        //print(" --- last index : \(self.parts.last?.index ?? -1)")
+    }
+    
+    func didEatApple(applePosition:CGSize) -> Bool{
+        
+        if let head = self.parts.first{
+            let height_diff = abs(head.position.height - applePosition.height)
+            let width_diff = abs(head.position.width - applePosition.width)
+            if height_diff < 20 && width_diff < 20 {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+        
+        
+    }
+    
+    func didEatItself() -> Bool { //
+        return false
+    }
+
+    func setApplePosition() -> CGSize{
+        
+        return CGSize.zero
+    }
+    
+    
+    
+}
