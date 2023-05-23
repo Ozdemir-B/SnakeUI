cbc5c8ceba43b07cb8f51fe6404118038aba6e32
Ozdemir-B <berkayozdemirv2@gmail.com>
2023-05-23 16:07:42 +0300

    Initial Commit

---
+++ SnakeUI/SnakeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeUIApp.swift
+//  SnakeUI
+//
+//  Created by Berkay Ozdemir on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnakeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
